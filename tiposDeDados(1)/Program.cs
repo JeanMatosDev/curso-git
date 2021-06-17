114c377c7820e4e05275df2be769e983066d2a87
Jean Clovis <jean.matos2.dev@gmail.com>
2021-06-17 13:08:43 -0300

    Projeto inicado monstrando Ola mundo

---
+++ tiposDeDados(1)/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace tiposDeDados_1_ {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Olá Mundo!");
+            bool completo = false;
+            char genero = 'F';
+            byte n1 = 126;
+            int n2 = 2147483647;
+            long n3 = 21474836445;
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(completo);
+            Console.WriteLine(genero);
+
+
+
+
+        }
+    }
+}
+
